d57edecbd92d8d5acc91b213f726801f034f5f8c
NatalyCortez <flyingchair_pe12@hotmail.com>
2018-01-07 21:42:56 -0500

    modificando jquery.js

--- js/jquery.js
+++ js/jquery.js
@@ -3,36 +3,54 @@ $(document).ready(function(){
 	var $tweetBtn = $('#tweet-btn');
 	var $messages = $('#messages');
 	var $counter = $('#count');
+	var $span = $('span');
 	var MAXCHARACTER = 140;
 
 	$tweetArea.on('input',function(){
-		var tweetCharacter = $(this).val().length;
-		console.log(tweetCharacter);
-	if(tweetCharacter > MAXCHARACTER){
-		alert('El texto es demasiado largo.');
-		$tweetBtn.prop('disabled',true);				
-		$tweetArea.val('');
-	} else if (tweetCharacter == false) {
+		var tweetCharacter = ($(this).val()).trim().length;		
+		var totalCharacters = MAXCHARACTER - tweetCharacter;		
+		$counter.text(totalCharacters);
+	if (tweetCharacter > MAXCHARACTER ) {
+		$tweetBtn.prop('disabled',true);
+	}
+	else if (tweetCharacter == false) {
 		$tweetBtn.prop('disabled',true);		
 	} else {
-		console.log('Usted puede publicar su post!.');
 		$tweetBtn.prop('disabled',false);
 	}
 	});
-	/*VERSIÓN 0.0.2*/
-	
 
+	if (tweetCharacter >= MAXCHARACTER - 10){
+		$counter.toggleClass('greenyellow');
+	}
+	else if (tweetCharacter >= MAXCHARACTER - 20){
+		$counter.toggleClass('seagreen');
+	}
+	else {
+		$counter.toggleClass('red');
+	}
+
+	
 	/*VERSIÓN 0.0.6 (EXTRA)*/
 	$tweetBtn.on('click',function(event){
 		event.preventDefault();
 		var textTweet = $tweetArea.val();
 		var $hour = moment().format('LT');
-	$(messages).append('<p>'+textTweet+ '<br>' + $hour +'</p>');
-		
-		$tweetArea.val('');		
-		$tweetBtn.prop('disabled',true);						
+		$(messages).prepend('<p>'+textTweet+ '<br>' + $hour +'</p>');
+		$tweetArea.val('');	
+		$tweetBtn.prop('disabled',true);
+		$counter.text(MAXCHARACTER);						
 	});
 });
 
+if (tweetCharacter >= MAXCHARACTER - 10){
+	$counter.toggleClass('greenyellow');
+}
+else if (tweetCharacter >= MAXCHARACTER - 20){
+	$counter.toggleClass('seagreen');
+}
+else {
+	$counter.toggleClass('red');
+}
 
 
